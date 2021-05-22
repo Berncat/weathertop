8f5220091d207e229c5d6152ccef4e87ab7701fb
Berncat <b.cattigan1@hotmail.com>
2021-05-22 14:02:30 +0100

    Pressure variable added in Reading Class, Latest Reading method added in Station, StationSummary Partial created and fomantic layout configured, Dashboard.html updated to include sataionsummary partial

--- app/models/Reading.java
+++ app/models/Reading.java
@@ -10,11 +10,13 @@ public class Reading extends Model
   public int code;
   public double temperature;
   public double windSpeed;
+  public int pressure;
   
-  public Reading(int code, double temperature, double windSpeed)
+  public Reading(int code, double temperature, double windSpeed, int pressure)
   {
     this.windSpeed = windSpeed;
     this.code = code;
     this.temperature = temperature;
+    this.pressure = pressure;
   }
 }
--- app/models/Station.java
+++ app/models/Station.java
@@ -10,14 +10,24 @@ import javax.persistence.OneToMany;
 import play.db.jpa.Model;
 
 @Entity
-public class Station extends Model
-{
+public class Station extends Model {
   public String name;
   @OneToMany(cascade = CascadeType.ALL)
   public List<Reading> readings = new ArrayList<Reading>();
 
-  public Station(String name)
-  {
+  public Station(String name) {
     this.name = name;
+  }
+
+  /*I found the subList method in the java api, I though I would use this method for latest reading
+  as you if you wished to evolve the app further you could maybe ask the user how many of the last readings
+  they would like to see.
+   */
+  public List<Reading> latestReadings() {
+    List<Reading> latestReadings = null;
+    if (readings.size() > 0) {
+      latestReadings = readings.subList(readings.size() - 1, readings.size());
+    }
+    return latestReadings;
   }
 }
